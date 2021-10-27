be8d7ab39c6fbb85035e0eab9587eb2319a874b9
Ivan <ivaann2706@gmail.com>
2021-10-27 17:20:05 +0200

    upload args exercise

---
+++ src/esnext/args.js
@@ -0,0 +1,25 @@
+function f(a, { b } = {}, c = 100) {
+  console.log(arguments.length);
+  console.log(a, a === arguments[0]);
+  console.log(b, b === arguments[1]);
+  console.log(c, c === arguments[2]);
+}
+
+console.log("*** Args ***");
+// Section A
+f("JS rocks!", { b: "b" });
+// 2
+// JS rocks! true
+// b false
+// 100 false
+
+// Section B
+f({ b: "b" });
+// 1
+// { b: 'b' } true
+// undefined true
+// 100 false
+
+// Section C
+f("JS sucks!", null, 13);
+// Uncaught TypeError: Cannot read properties of null (reading 'b')
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import "./fundamentals/exercises.js";
+import "./esnext/exercises.js";
 
 console.log("*** Welcome to Exercises Playground. ***");
