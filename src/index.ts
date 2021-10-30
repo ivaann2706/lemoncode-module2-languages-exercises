348bab03204d167ca9c3a0ae51ee973f0d92a24d
Ivan <ivaann2706@gmail.com>
2021-10-30 13:53:19 +0200

    only run the deliverable exercises

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "./00 practice/exercise.js";
+// import "./00 practice/exercise.js";
 
 import "./01 deliverables/exercise-01.js";
 import "./01 deliverables/exercise-02.js";
@@ -6,4 +6,4 @@ import "./01 deliverables/exercise-03.js";
 import "./01 deliverables/exercise-04.js";
 import "./01 deliverables/exercise-05.js";
 
-import "./02 challenges/exercise.js";
+// import "./02 challenges/exercise.js";
