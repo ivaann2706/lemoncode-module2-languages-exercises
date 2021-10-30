9c96b9055e74a1c9a5773bb2f9b3c61b4a1561f7
Ivan <ivaann2706@gmail.com>
2021-10-30 12:44:07 +0200

    update array-operations exercise

--- src/00 practice/esnext/array-operations.js
+++ src/00 practice/esnext/array-operations.js
@@ -1,14 +1,19 @@
 console.log("*** Array Operations ***");
 
+const array1 = [1, 2, 3, 4, 5];
+const array2 = ["one", "two", "three"];
+console.log("array1", array1);
+console.log("array2", array2);
+
 // Apartado A
 // Implementa una función `head` (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
 const head = (array) => {
   const [first] = array;
   return first;
 };
-console.log(head([1, 2, 3, 4, 5])); // 1
-console.log(head(["one", "two", "three"])); // one
-console.log(head("one")); // o
+console.log("--> head");
+console.log("head(array1):", head(array1)); // 1
+console.log("head(array2):", head(array2)); // one
 
 // Apartado B
 // Implementa una función `tail` (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
@@ -16,20 +21,20 @@ const tail = (array) => {
   const [, ...tail] = array;
   return tail;
 };
-console.log(tail([1, 2, 3, 4, 5])); // [2, 3, 4, 5]
-console.log(tail(["one", "two", "three"])); // ['two', 'three']
-console.log(tail("one")); // ne
+console.log("--> tail");
+console.log("tail(array1):", tail(array1)); // [2, 3, 4, 5]
+console.log("tail(array2):", tail(array2)); // ['two', 'three']
 
 // Apartado C
 // Implementa una función `init` (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece `Array.prototype`.
 const init = (array) => array.slice(0, -1); // Implementation here.
-console.log(init([1, 2, 3, 4, 5])); // [1, 2, 3, 4]
-console.log(init(["one", "two", "three"])); // ['one', two']
-console.log(init("one")); // on
+console.log("--> init");
+console.log("init(array1):", init(array1)); // [1, 2, 3, 4]
+console.log("init(array2):", init(array2)); // ['one', two']
 
 // Apartado D
 // Implementa una función `last` (inmutable), tal que, dado un array como entrada devuelva el último elemento.
 const last = (array) => array[array.length - 1];
-console.log(last([1, 2, 3, 4, 5])); // 5
-console.log(last(["one", "two", "three"])); // three
-console.log(last("one")); // e
+console.log("--> last");
+console.log("last(array1):", last(array1)); // 5
+console.log("last(array2):", last(array2)); // three
