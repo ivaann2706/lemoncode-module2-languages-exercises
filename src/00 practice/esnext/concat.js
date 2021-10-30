41eacffd15bd75d28bc35749e075b2ebb7db9036
Ivan <ivaann2706@gmail.com>
2021-10-30 12:57:14 +0200

    update concat exercise

--- src/00 practice/esnext/concat.js
+++ src/00 practice/esnext/concat.js
@@ -4,8 +4,7 @@ console.log("*** Concat ***");
 // Implementa una función `concat`, tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. Utiliza rest / spread.
 //**TIP**: Se hace en 1 línea. No utilices el método `Array.prototype.concat()`.
 const concat = (a, b) => [...a, ...b]; // Implementation here.
-console.log(concat([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
-console.log(concat(["one", "two", "three"], ["four", "five", "six"])); // ['one', 'two', 'three', 'four', 'five', 'six']
+console.log("concat([1, 2, 3], [4, 5, 6]):", concat([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
 
 // Apartado B
 // Repite el ejercicio anterior suponiendo cualesquiera arrays de entrada (no te limites solamente a 2).
@@ -16,15 +15,10 @@ const concatMulti = (...a) => {
   });
   return result;
 };
-console.log(concatMulti([1, 2, 3], [4, 5, 6], [7], [8, 9])); // [1, 2, 3, 4, 5, 6, 7, 8 , 9]
 console.log(
-  concatMulti(
-    ["one", "two", "three"],
-    ["four", "five", "six"],
-    ["seven"],
-    ["eight", "nine"]
-  )
-); // ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  "concatMulti([1, 2], [3], [4], [5, 6]):",
+  concatMulti([1, 2], [3], [4], [5, 6])
+); // [1, 2, 3, 4, 5, 6]
 
 const concatMulti2Recursive = (...a) => {
   if (a.length === 0) {
@@ -33,12 +27,7 @@ const concatMulti2Recursive = (...a) => {
   const [first, ...tail] = a;
   return [...first, ...concatMulti2Recursive(...tail)];
 };
-console.log(concatMulti2Recursive([1, 2, 3], [4, 5, 6], [7], [8, 9])); // [1, 2, 3, 4, 5, 6, 7, 8 , 9]
 console.log(
-  concatMulti2Recursive(
-    ["one", "two", "three"],
-    ["four", "five", "six"],
-    ["seven"],
-    ["eight", "nine"]
-  )
-); // ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  "concatMulti2Recursive([1, 2], [3], [4], [5, 6])",
+  concatMulti2Recursive([1, 2], [3], [4], [5, 6])
+); // [1, 2, 3, 4, 5, 6]
