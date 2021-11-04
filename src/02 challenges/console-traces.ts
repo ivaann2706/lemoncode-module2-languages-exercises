ecd8c4011551c0119efcec7a2bcb931afa44b4b5
Ivan <ivaann2706@gmail.com>
2021-11-04 10:13:45 +0100

    upload console-traces exercise

---
+++ src/02 challenges/console-traces.ts
@@ -0,0 +1,29 @@
+// @ts-nocheck
+console.log("*** Console Traces ***");
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const showMessage = async ([time, message]: [number, string]) => {
+  await delay(time);
+  console.log(message);
+};
+
+const triggers = [
+  async () => await showMessage([200, "third"]),
+  async () => await showMessage([100, "second"]),
+];
+
+const run = (triggers) => {
+  console.log("first");
+  triggers.forEach((t) => t());
+};
+
+const run2 = async (triggers) => {
+  for (const trigger of triggers) {
+    await trigger();
+  }
+  console.log("first");
+};
+
+// run(triggers);
+run2(triggers);
--- src/02 challenges/exercise.ts
+++ src/02 challenges/exercise.ts
@@ -1,1 +1,1 @@
-
+import "./console-traces.js";
