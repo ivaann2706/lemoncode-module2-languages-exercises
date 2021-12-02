925e9be955e069aed79a68601bc2f18c494ed208
Ivan <ivaann2706@gmail.com>
2021-12-02 23:56:13 +0100

    upload tree exercise

--- src/02 challenges/exercise.ts
+++ src/02 challenges/exercise.ts
@@ -2,3 +2,4 @@ import "./console-traces.js";
 import "./deep-access.js";
 import "./flatten-array.js";
 import "./memoization.js";
+import "./tree.js";
---
+++ src/02 challenges/tree.ts
@@ -0,0 +1,67 @@
+console.log("*** Tree ***");
+
+interface Nodo<T> {
+  name: T;
+  children: Nodo<T>[];
+  parent?: Nodo<T>;
+}
+
+interface Tree<T> {
+  root: Nodo<T>;
+}
+
+const isRoot = <T>(nodo: Nodo<T>): boolean => {
+  return !nodo.parent;
+};
+
+const isLeaf = <T>(nodo: Nodo<T>): boolean => {
+  return nodo.children.length === 0;
+};
+
+const addChild = <T>(nodo: Nodo<T>, name: T): void => {
+  const child = {
+    name: name,
+    parent: nodo,
+    children: [],
+  };
+  nodo.children = [...nodo.children, child];
+};
+
+const getParent = <T>(nodo: Nodo<T>): Nodo<T> | undefined => {
+  if (isRoot(nodo)) {
+    console.log("This node is the root and has no parent");
+    return undefined;
+  } else {
+    return nodo.parent;
+  }
+};
+
+const getChildren = <T>(nodo: Nodo<T>): Nodo<T>[] => {
+  if (isLeaf(nodo)) {
+    console.log("This node is a leaf and has no children");
+  }
+  return nodo.children;
+};
+
+const nodo_root: Nodo<string> = {
+  name: "raíz",
+  children: [],
+};
+
+const tree: Tree<string> = {
+  root: nodo_root,
+};
+
+addChild(nodo_root, "1");
+addChild(nodo_root, "2");
+addChild(nodo_root, "3");
+
+const children = getChildren(nodo_root);
+
+children.forEach((child) => {
+  const name = `${child.name}-1`;
+  addChild(child, name);
+});
+
+console.log("isRoot(nodo_root)", isRoot(nodo_root));
+console.log("isLeaf(nodo_root)", isLeaf(nodo_root));
